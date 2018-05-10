8bbfeb11e10ff484620e8375fac91c7d031f6886
rx1105 <nswodewy@yeah.net>
2018-05-10 21:55:10 +0800

    add favicon

--- TianTianDir/tiantianProject/tiantianProject/urls.py
+++ TianTianDir/tiantianProject/tiantianProject/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
+
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^user/', include('df_user.urls'))
+    url(r'^user/', include('df_user.urls')),
+    url(r'^favicon\.ico$', favicon_view)
 ]
